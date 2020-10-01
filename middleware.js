8a77eb0d80b659fb3bb5edfb2419dc4a976b3460
arjun-369 <68965236+arjun-369@users.noreply.github.com>
2020-10-01 11:17:43 +0530

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,36 @@
+let  jwt= require('jsonwebtoken');
+const config= require('./config.js');
+
+let checkToken =(req,res,next)=>{
+    let token =req.headers['x-access-token'] || req.headers['authorization'] || '';//express header
+
+    if(token){
+        if(token.startsWith('Bearer ')){
+            token=token.slice(7,token.length)//remove bearer from string
+        }
+
+        jwt.verify(token,config.secret,(err,decoded) =>{
+            if(err){
+                return res.json({
+                    success: false,
+                    message: 'Token is not valid'
+                });
+            }
+            else{
+                req.decoded=decoded;
+                next();
+            }
+        });
+
+    }
+    else{
+        return res.json({
+            success: false,
+            message: 'Auth token is not supplied'
+        });
+    }
+};
+
+module.exports={
+    checkToken: checkToken
+};
